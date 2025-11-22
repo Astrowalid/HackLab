ec2b9fcafe70c250ba3b15e883ddfa804b969f28
Abdelmajid Hsairi <abdelmajidhsairi@gmail.com>
2025-11-23 00:53:29 +0100

    Profile Screen

--- app/src/main/java/com/example/hacklab/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/hacklab/screens/ProfileScreen.kt
@@ -1,2 +1,190 @@
 package com.example.hacklab.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.hacklab.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.ui.unit.sp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Profile(navController: NavController){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF0A0A12)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.default_user),
+                contentDescription = "Profile picture",
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+            Text(
+                "John Doe",
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+
+            Text(
+                "john.doe@email.com",
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Gray,
+                modifier = Modifier.padding(top = 4.dp)
+            )
+
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF242847),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+            ) {
+                Text("Edit Profile")
+            }
+        }
+
+        SettingsList(Modifier.padding(horizontal = 16.dp))
+
+        Column(
+            modifier = Modifier.padding(16.dp),
+        ){
+
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFDE0000),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+            ) {
+                Text("Log Out")
+            }
+
+        }
+
+    }
+}
+
+
+@Composable
+fun SettingsList(modifier: Modifier = Modifier) {
+    val settingsItems = listOf(
+        SettingItem("Notifications"),
+        SettingItem("Confidentialité"),
+        SettingItem("Sécurité"),
+        SettingItem("Apparence"),
+        SettingItem("Aide & Support")
+    )
+
+    Column(modifier = modifier) {
+        Text(
+            text = "Settings",
+            fontSize = 19.sp,
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        settingsItems.forEach { item ->
+            SettingRow(
+                item = item,
+                onClick = {  }
+            )
+        }
+    }
+}
+
+@Composable
+fun SettingRow(item: SettingItem, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 7.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+        onClick = onClick
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                fontSize = 15.sp,
+                text = item.title,
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.White
+            )
+            Icon(
+                imageVector = Icons.Default.ChevronRight,
+                contentDescription = "Aller vers ${item.title}",
+                tint = Color.Gray
+            )
+        }
+    }
+}
+
+data class SettingItem(val title: String)
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreen(){
+    Profile(navController = rememberNavController())
+}
