09990e573600d2506ec748c1f43d0dc995df9e6b
Abdelmajid Hsairi <abdelmajidhsairi@gmail.com>
2025-11-26 17:38:56 +0100

    l'ajoute de systeme du panier

--- app/src/main/java/com/example/hacklab/data/CartItem.kt
+++ app/src/main/java/com/example/hacklab/data/CartItem.kt
@@ -1,3 +1,10 @@
-
 package com.example.hacklab.data
 
+data class CartItem(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val imageResId: Int,
+    val quantity: Int = 1
+)
