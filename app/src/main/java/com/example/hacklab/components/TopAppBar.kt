4d87a5ff4b5ec20007b30c3a868c67eea94b2990
Abdelmajid Hsairi <abdelmajidhsairi@gmail.com>
2025-11-26 17:40:51 +0100

    modification de TopNavigationBar

--- app/src/main/java/com/example/hacklab/components/TopAppBar.kt
+++ app/src/main/java/com/example/hacklab/components/TopAppBar.kt
@@ -1,66 +1,60 @@
 package com.example.hacklab.components
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.hacklab.R
-import com.example.hacklab.navigation.AppNavigation
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopNavigationBar(navController: NavController) {
+fun TopNavigationBar(navController: NavController,title:String) {
     TopAppBar(
-        title = {},
-        navigationIcon = {
-            IconButton(
-                onClick = {
-                    navController.navigate(AppNavigation.ProductList.route) {
-                        popUpTo(AppNavigation.ProductList.route) { inclusive = true }
-                    }
-                }
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.hacklab_white),
-                    contentDescription = "Logo",
-                    modifier = Modifier.size(40.dp)
-                )
-            }
-        },
-        actions = {
-            IconButton(
-                onClick = {
-                    navController.navigate(AppNavigation.Card.route)
-                }
-            ) {
-                Icon(
-                    imageVector = Icons.Default.ShoppingCart,
-                    contentDescription = "Cart",
-                    tint = Color.White
-                )
-            }
-        },
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color(0xFF0A0A12)
-        )
+            containerColor = Color(0xFF0A0A12),
+            titleContentColor = Color.White
+        ),
+        title = { Text(
+            title,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        ) },
+        navigationIcon = {
+            IconButton(onClick = {
+                navController.popBackStack()
+            }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Back",tint = Color.White)
+            }
+        }
+
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun TopNavigationBarPreview() {
-    TopNavigationBar(navController = rememberNavController())
+fun TopNavigationBarTypePreview() {
+    TopNavigationBarType2(navController = rememberNavController(),title = "Preview")
 }
